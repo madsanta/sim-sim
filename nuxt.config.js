876f039cdd8f2f5c6296d33f8c3e141d45a939ac
Aleksandr Golyshev <asgolyshev@avito.ru>
2024-09-11 01:08:26 +0400

    feat(all): remove auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     // Target: https://go.nuxtjs.dev/config-target
     target: 'static',
     router: {
-        middleware: 'auth',
+        // middleware: 'auth',
         base: '/sim-sim/'
     },
 
